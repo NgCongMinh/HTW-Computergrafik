4eb89746090da31eb4aa7cb95cade707b2fc042e
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2019-06-27 22:09:13 +0200

    client - menu, server communication, ...

---
+++ Pingpong_Client/Assets/Scripts/Network/NetworkIdentity.cs
@@ -0,0 +1,49 @@
+﻿using SocketIO;
+using UnityEngine;
+
+namespace Network
+{
+    public class NetworkIdentity : MonoBehaviour
+    {
+        [Header("Helpful Values")] [SerializeField]
+        private string id;
+
+        [SerializeField]
+        private bool isControlling;
+
+        private SocketIOComponent socket;
+
+        // Start is called before the first frame update
+        public void Awake()
+        {
+            isControlling = false;
+        }
+
+        public void SetControllerId(string id)
+        {
+            this.id = id;
+            isControlling = (NetworkClient.ClientId == id) ? true : false;
+        }
+
+        public void SetSocket(SocketIOComponent socket)
+        {
+            this.socket = socket;
+        }
+
+        public string GetId()
+        {
+            return id;
+        }
+
+        public bool IsControlling()
+        {
+            return isControlling;
+        }
+        
+        public SocketIOComponent GetSocket()
+        {
+            return socket;
+        }
+        
+    }
+}
---
+++ Pingpong_Client/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Network;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    [Header("Data")]
+    [SerializeField]
+    private float speed = 50;
+
+    [Header("Class Reference")]
+    [SerializeField]
+    private NetworkIdentity networkIdentity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (networkIdentity.IsControlling())
+        {
+            CheckMovement();
+        }
+    }
+
+    private void CheckMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+           
+        transform.position += new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+    }
+    
+}
